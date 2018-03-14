d9beeea478a16eae1933f532ec1d8d132fc34aad
Mark Featherston <mark@embeddedarm.com>
2018-03-14 12:21:05 -0700

    ts7800v2_wdt: Initial driver commit.  Used to feed microcontroller based watchdog over i2c on TS-7800-V2, TS-7820, TS-7840.

---
+++ drivers/watchdog/ts7800-v2_wdt.c
@@ -0,0 +1,268 @@
+/*
+ * Copyright (C) Technologic Systems, Inc.
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/reboot.h>
+#include <linux/watchdog.h>
+#include <linux/platform_device.h>
+#include <linux/reset-controller.h>
+#include <linux/ioctl.h>
+#include <asm/uaccess.h>
+
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/i2c.h>
+
+
+#define DEVNAME "ts7800v2-wdt"
+
+static struct i2c_client *i2c_client;
+DEFINE_MUTEX(wdtlock);
+static unsigned int wdt_active;
+static bool nowayout = WATCHDOG_NOWAYOUT;
+module_param(nowayout, bool, 0);
+MODULE_PARM_DESC(nowayout, "Watchdog cannot be stopped once started (default="
+				__MODULE_STRING(WATCHDOG_NOWAYOUT) ")");
+
+static int do_ts7800v2_wdt_write(unsigned int val)
+{
+	unsigned char data[6];
+	int ret;
+
+	if (val == 0) {
+		data[0] = 0x04; data[1] = 0x04; /* reg address is 0x404, or 1028 decimal*/
+		*(unsigned int*)&data[2] = 0;
+
+	} else {
+		data[0] = 0x04; data[1] = 0; /* reg address is 0x400, or 1024 decimal */
+		*(unsigned int*)&data[2] = val * 100;
+	}
+
+	ret = i2c_master_send(i2c_client, data, sizeof(data));
+
+	if (ret != sizeof(data))
+		return -1;
+	return 0;
+}
+
+static int ts7800v2_wdt_write(unsigned int val)
+{
+	int ret = 0;
+	mutex_lock(&wdtlock);
+	ret = do_ts7800v2_wdt_write(val);
+	mutex_unlock(&wdtlock);
+	return ret;
+}
+
+static int ts7800v2_wdt_start(struct watchdog_device *wdt)
+{
+	unsigned char data[6] = {
+		0x04,0x04,  /* reg address is 0x404, or 1028 decimal */
+		1,0,0,0     /* Write 1 to enable the wdt */
+	};
+
+	ts7800v2_wdt_write(wdt->timeout + 1);
+	wdt_active = 1;
+	if (i2c_master_send(i2c_client, data, sizeof(data)) != sizeof(data))
+		return -1;
+	return 0;
+}
+
+static int ts7800v2_wdt_stop(struct watchdog_device *wdt)
+{
+	if (nowayout)
+		return 0;
+	ts7800v2_wdt_write(0);
+	wdt_active = 0;   /* disabled */
+	return 0;
+}
+
+static int ts7800v2_wdt_ping(struct watchdog_device *wdt)
+{
+  unsigned char data[6] = {
+		0x04,0x04,  /* reg address is 0x404, or 1028 decimal */
+		1,0,0,0     /* Write 1 to enable/feed the wdt */
+  };
+
+  if (i2c_master_send(i2c_client, data, sizeof(data)) != sizeof(data))
+		return -1;
+  return 0;
+}
+
+static int ts7800v2_wdt_set_timeout(struct watchdog_device *wdt,
+					unsigned int timeout)
+{
+	wdt->timeout = timeout;
+	ts7800v2_wdt_write(timeout);
+	return 0;
+}
+
+static unsigned int ts7800v2_wdt_status(struct watchdog_device *wdt)
+{
+	unsigned int ret = 0;
+
+	if (wdt_active) ret = WDOG_ACTIVE;
+	if (nowayout) ret |= WDOG_NO_WAY_OUT;
+	return ret;
+}
+
+static const struct watchdog_info ts7800v2_wdt_info = {
+	.options = WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING,
+	.identity = DEVNAME,
+	.firmware_version = 1,
+};
+
+static const struct watchdog_ops ts7800v2_wdt_ops = {
+	.owner		= THIS_MODULE,
+	.start		= ts7800v2_wdt_start,
+	.stop		= ts7800v2_wdt_stop,
+	.ping = ts7800v2_wdt_ping,
+	.status = ts7800v2_wdt_status,
+	.set_timeout	= ts7800v2_wdt_set_timeout,
+};
+
+static int wdog_panic_handler(struct notifier_block *this,
+			      unsigned long         event,
+			      void                  *unused)
+{
+	static int panic_event_handled = 0;
+
+	if (!panic_event_handled) {
+		/* Make sure we do this only once. */
+		panic_event_handled = 1;
+
+		/* Arm WDT for 2 seconds instea dof instantly so there is time for the panic
+		 * to output on the serial console. */
+		mutex_lock(&wdtlock);
+		do_ts7800v2_wdt_write(200);
+		while(1);
+	}
+
+	return NOTIFY_OK;
+}
+
+static struct notifier_block wdog_panic_notifier = {
+	.notifier_call	= wdog_panic_handler,
+	.next		= NULL,
+	.priority	= 150	/* priority: INT_MAX >= x >= 0 */
+};
+
+static int wdog_reboot_handler(struct notifier_block *this,
+			       unsigned long         code,
+			       void                  *unused)
+{
+	mutex_lock(&wdtlock);
+	if(code == SYS_RESTART){
+		/* Reset in 10ms */
+		do_ts7800v2_wdt_write(1);
+	} else {
+		/* Stall forever. */
+		do_ts7800v2_wdt_write(0);
+	}
+	while(1);
+	return NOTIFY_OK;
+}
+
+static struct notifier_block wdog_reboot_notifier = {
+	.notifier_call	= wdog_reboot_handler,
+	.next		= NULL,
+	.priority	= 0
+};
+
+static int ts7800v2_wdt_probe(struct i2c_client *client,
+					const struct i2c_device_id *id)
+{
+	int ret = 0;
+	struct watchdog_device *wdt;
+
+	i2c_client = client;
+
+	wdt = devm_kzalloc(&client->dev, sizeof(*wdt), GFP_KERNEL);
+	if (!wdt)
+		return -ENOMEM;
+
+	wdt->info	= &ts7800v2_wdt_info;
+	wdt->ops		= &ts7800v2_wdt_ops;
+	wdt->status		= 0;
+	wdt->timeout		= 60;
+	wdt->min_timeout	= 1;
+	wdt->max_timeout	= 1000;
+	wdt->parent = &client->dev;
+
+	i2c_set_clientdata(client, wdt);
+	watchdog_init_timeout(wdt, wdt->timeout, &client->dev);
+	watchdog_set_nowayout(wdt, nowayout);
+	watchdog_set_drvdata(wdt, NULL);
+	ts7800v2_wdt_stop(wdt);
+
+	ret = watchdog_register_device(wdt);
+	if (ret)
+		return ret;
+
+	register_reboot_notifier(&wdog_reboot_notifier);
+	atomic_notifier_chain_register(&panic_notifier_list,
+			&wdog_panic_notifier);
+
+	ts7800v2_wdt_start(wdt);
+
+	return 0;
+}
+
+static int ts7800v2_wdt_remove(struct i2c_client *client)
+{
+	struct watchdog_device *wdt = i2c_get_clientdata(client);
+
+	watchdog_unregister_device(wdt);   return 0;
+}
+
+
+static const struct of_device_id ts7800v2_wdt_of_match[] = {
+	{ .compatible = "technologicsystems,ts7800v2-wdt", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, ts7800v2_wdt_of_match);
+
+static const struct i2c_device_id ts7800v2_wdt_id[] = {
+	{DEVNAME, 0},
+	{},
+};
+
+MODULE_DEVICE_TABLE(i2c, ts7800v2_wdt_id);
+
+static struct i2c_driver ts7800v2_wdt_driver = {
+	.probe		= ts7800v2_wdt_probe,
+	.remove		= ts7800v2_wdt_remove,
+	.id_table = ts7800v2_wdt_id,
+	.driver		= {
+		.name		= DEVNAME,
+		.of_match_table	= ts7800v2_wdt_of_match,
+	},
+};
+
+module_i2c_driver(ts7800v2_wdt_driver);
+
+MODULE_AUTHOR("Technologic Systems Inc");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("platform:ts7800v2-wdt");
+
